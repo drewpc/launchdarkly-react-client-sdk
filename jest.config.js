2a96554afa2d48a5af3e265c1dcd0ae24eaea055
PC Drew <pdrew@pagerduty.com>
2026-02-16 15:57:20 -0700

    Add test report analytics

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,12 @@ module.exports = {
   reporters: [
     'default',
     ["jest-junit", {
+      "outputDirectory": "test-reports/junit/",
       "outputName": "junit.xml"
     }]
   ],
+  collectCoverage: true,
+  coverageDirectory: "test-reports/coverage",
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
     '\\.(ts|tsx)$': 'ts-jest',
